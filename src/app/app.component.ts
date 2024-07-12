1a1f705d8bae777780e2631cd18f62ed4c13817d
michaelis <gregoriades.michaelis@gmail.com>
2024-07-13 00:45:36 +0200

    init firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormBuilder } from '@angular/forms';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ihm';
+
+  thing: any;
+
+  testForm = this.fb.group({
+    test: [],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private store: AngularFirestore,
+  ) {
+  }
+
+  onTest() {
+    console.log(this.testForm.get('test')!.value);
+    const ref = this.store.collection('/test');
+    ref.snapshotChanges()
+    .pipe(
+      map(t => t.map(
+        c => ({ id: c.payload.doc.id, data: c.payload.doc.data() }),
+      )),
+    )
+    .subscribe(console.log);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatInputModule } from '@angular/material/input';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
 
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatInputModule,
+
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
