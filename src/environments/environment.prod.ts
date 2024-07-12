4a7b8311f4bc001cf39de68263f174b526f9500d
michaelis <gregoriades.michaelis@gmail.com>
2024-07-12 17:33:03 +0200

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyBznykbYLZ12rB-ltLMU_WF39q59D9eAl8",
+    authDomain: "branquignoleurs-668ea.firebaseapp.com",
+    projectId: "branquignoleurs-668ea",
+    storageBucket: "branquignoleurs-668ea.appspot.com",
+    messagingSenderId: "606456430389",
+    appId: "1:606456430389:web:7f814ae239501804a9167c"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyBznykbYLZ12rB-ltLMU_WF39q59D9eAl8",
+    authDomain: "branquignoleurs-668ea.firebaseapp.com",
+    projectId: "branquignoleurs-668ea",
+    storageBucket: "branquignoleurs-668ea.appspot.com",
+    messagingSenderId: "606456430389",
+    appId: "1:606456430389:web:7f814ae239501804a9167c"
+  },  
 };
 
 /*
